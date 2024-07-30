182925b614c0ba8b0ef0823c6cd7641f15a04b6d
seshubonam <seshu.bonam@gmail.com>
2024-07-30 06:04:19 +0530

    Update workflow.py

--- src/workflow.py
+++ src/workflow.py
@@ -96,7 +96,7 @@ async def stream_workflow(
         'Content-Type': 'application/json'
     }
     json = {"input": msg_data, "sessionId": thread_id,
-            "enableStreaming": False, "stream_token": False}
+            "enableStreaming": True, "stream_token": True}
     if user_email:
         json["userEmail"] = user_email
     logger.info(f"stream json : {json}")
